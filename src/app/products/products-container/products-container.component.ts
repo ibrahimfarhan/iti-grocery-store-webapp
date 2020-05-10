92a49a0168a6161c64649b4fc9ddfa90256b6d03
IbrahimAbdelrahman <45977888+IbrahimAbdelrahman@users.noreply.github.com>
2020-05-10 13:12:40 +0200

    AddingBootstrapAndFontawesome

--- src/app/products/products-container/products-container.component.ts
+++ src/app/products/products-container/products-container.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-products-container',
   templateUrl: './products-container.component.html',
-  styleUrls: ['./products-container.component.scss']
+  styleUrls: ['./products-container.component.scss'],
 })
 export class ProductsContainerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  
+  ngOnInit(): void {}
 }

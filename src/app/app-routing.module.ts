3c6cfb0e44d7744362ff53503bcd4f5ab9587614
Hossam <hossameldinali96@gmail.com>
2020-05-15 05:45:00 +0200

    Add a Route for CategoriesList in AppRoutingModule
    Included the previously created resolver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CategoriesListComponent } from './products/categories-list/categories-list.component';
+import { CategoriesListResolver } from './products/categories-list-resolver.service';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'categories', component: CategoriesListComponent, resolve: {categories: CategoriesListResolver}}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

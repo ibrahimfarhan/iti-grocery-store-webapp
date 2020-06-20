f879766f03af317029b542b49b00cc8dd84832cc
Hossam <hossameldinali96@gmail.com>
2020-06-20 22:15:27 +0200

    Update ProductsContainer Component

--- src/app/products/products-container/products-container.component.ts
+++ src/app/products/products-container/products-container.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { Product } from '../../models/product';
 import { ProductService } from '../../services/product.service';
-import { ActivatedRoute } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -15,20 +16,24 @@ export class ProductsContainerComponent implements OnInit {
   errorMessage: string;
 
   constructor(private productService: ProductService,
-              private route: ActivatedRoute,
-              private authService: AuthService) { }
+    private route: ActivatedRoute,
+    private authService: AuthService) { }
 
   ngOnInit(): void {
-    this.route.data.subscribe({
-      next: data => this.onProductRetrieved(data.resolvedProducts) ,
-      error: msg =>  this.errorMessage = msg
+    this.productService.getProducts().subscribe({
+      next: data => this.onProductRetrieved(data),
+      error: msg => this.errorMessage = msg
     });
+
+    // this.route.data.subscribe({
+    //   next: data => this.onProductRetrieved(data.resolvedProducts),
+    //   error: msg => this.errorMessage = msg
+    // });
   }
 
-  onProductRetrieved(retrievedProducts: Product[]): void
-  {
-    // if it's != null
-    if (!retrievedProducts){
+  onProductRetrieved(retrievedProducts: Product[]): void {
+    // if it's == null
+    if (!retrievedProducts) {
       this.errorMessage = 'There are no products found for this category';
     }
     this.products = retrievedProducts;
@@ -36,6 +41,6 @@ export class ProductsContainerComponent implements OnInit {
   // checks if the user who loggedin is admin to display edit icon
   isAdmin(): boolean {
     // call auth service
-    return this.authService.isAdmin();
+    return !this.authService.isAdmin();
   }
 }

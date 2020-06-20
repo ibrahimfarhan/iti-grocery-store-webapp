47eb29d60652764dd8094fcb1ccfd18e3d67e5dd
Hossam <hossameldinali96@gmail.com>
2020-06-20 17:45:15 +0200

    App Module Code Clean up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { CategoriesListComponent } from './products/categories-list/categories-list.component';
-import { CategoriesListResolver } from './services/categories-list-resolver.service';
 import { HomeComponent } from './core/home/home.component';
-import { ProductsPageComponent } from './products/products-page/products-page.component';
-import { ProductsContainerComponent } from './products/products-container/products-container.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -14,14 +10,7 @@ const routes: Routes = [
     path: 'products',
     loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
   },
-  { // this should be moved in products routings 
-    path: 'categories', component: CategoriesListComponent, resolve: {categories: CategoriesListResolver},
-    children: [
-      { path: '', component: ProductsContainerComponent, pathMatch: 'full'},
-      { path: ':d', component: ProductsContainerComponent },
-    ]
-  },
-  { 
+  {
     path: 'admin',
     loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
   },
@@ -34,4 +23,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { ErrorInterceptorProvider } from './interceptors/error.interceptor';
 import { SharedModule } from './shared/shared.module';
+import { ErrorInterceptorProvider } from './interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
